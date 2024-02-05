729eeb9cc3ce8b06a2ae19ea1c54a3c8ee9cadb3
Nandit <nanditshah105@gmail.com>
2024-02-05 17:14:38 +0530

    UI task for Qlick calulator implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+const buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 function App() {
+  const [expression, setExpression] = useState("");
+  const [ans, setAns] = useState();
+  const handleExpressionChange = (e) => {
+    setExpression(e.target.value);
+  };
+  const appendExpression = (expression) => {
+    setExpression((prev) => (prev += "" + expression));
+  };
+
+  const handleButtonClick = async () => {
+    console.log(expression);
+    await calculate();
+  };
+
+  const calculate = async () => {
+    axios
+      .post("http://localhost:8000/calculate", { expression })
+      .then((res) => {
+        if (res.status === 200) {
+          setAns(res.data);
+          setExpression(res.data);
+        }
+      });
+  };
+
+  const handleKeyDown = async (e) => {
+    console.log(e.keyCode);
+    if (e.keyCode >= 48 && e.keyCode >= 58) {
+      setExpression((prev) => (prev += "" + (e.keyCode % 48)));
+    } else if (e.keyCode === 13) {
+      await calculate();
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onKeyDown={handleKeyDown}>
+      <input value={expression} onChange={handleExpressionChange} />
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        {buttons.map((button) => (
+          <button onClick={() => appendExpression(button)}>{button}</button>
+        ))}
+      </div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <button onClick={() => appendExpression("+")}>+</button>
+        <button onClick={() => appendExpression("-")}>-</button>
+        <button onClick={() => appendExpression("*")}>*</button>
+        <button onClick={() => appendExpression("/")}>/</button>
+      </div>
+      <button onClick={handleButtonClick}>Get anshwer (=)</button>
+      <h1>Answer is - {ans}</h1>
     </div>
   );
 }
